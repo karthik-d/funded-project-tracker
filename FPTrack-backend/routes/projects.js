4c4812f5ac4db691fcec2c8cfff3cf3690dd0fbf
karthik-d <karthikdesingu2000@gmail.com>
2022-05-17 15:30:33 +0530

    DB connection partial config==

--- FPTrack-backend/routes/projects.js
+++ FPTrack-backend/routes/projects.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+    const dbConnect = dbo.getDb();
   res.send('respond with a resource');
 });
 
