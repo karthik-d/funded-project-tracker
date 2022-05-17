eee50add7b6ad507526e165a8c1c01a8b588c652
karthik-d <karthikdesingu2000@gmail.com>
2022-05-17 19:57:18 +0530

    migrates moved, connection redundancy removed

--- FPTrack-backend/db/migrations/1652793703064-make-first-admin.js
+++ FPTrack-backend/db/migrations/1652793703064-make-first-admin.js
@@ -1,4 +1,4 @@
-const { User } = require('../models')
+const { User } = require('../../models')
 
 async function up() {
   await User.create(
--- FPTrack-backend/models/index.js
+++ FPTrack-backend/models/index.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const User = require('./user');
-
-var mongoDB = 'mongodb+srv://fpt_root:cchZf36i4TBv6GlK@fpt-cluster-j2k2.f81v7.mongodb.net/FPT_DB?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+const connection = require('../db/connect');
 
 module.exports = {
     User
