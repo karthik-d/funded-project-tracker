b8ee4be540ed94721c0b1deeec2da131baf971f7
karthik-d <karthikdesingu2000@gmail.com>
2022-05-31 16:32:07 +0530

    extended multi-object pass for project render

--- FPTrack-frontend/pages/guest/view_projects.js
+++ FPTrack-frontend/pages/guest/view_projects.js
@@ -7,90 +7,94 @@ import React, { useState, useEffect, Component } from 'react';
 import user from '../../../FPTrack-backend/models/user';
 
 
-function Tabulate({props}){
+function Tabulate({ props }) {
 	console.log("HERE--?");
 	console.log(typeof props.objects[0]);
-    return (<div className = {styles.tabulate}>
-    <table>
-      <thead id ="">
-        <tr id="header" key = "header">
-        {props.keys.map(key=>{
-          return (<th key = {key}>{key}</th>);
-        })}
-        </tr>
-      </thead>
-      <tbody id="objects">
-      {		  
-		  props.objects[0].map(obj=>{ 
-			return (
-				<tr key={obj._id}>
+	return (<div className={styles.tabulate}>
+		<table>
+			<thead id="">
+				<tr id="header" key="header">
+					{props.keys[0].map(key => {
+						return (<th key={key}>{key}</th>);
+					})}
+				</tr>
+			</thead>
+			<tbody id="objects">
 				{
-					props.keys[0].map(key=>{
-						return (<td key = {key}>{obj[key]}</td>);
+					[...props.objects.keys()].map(idx => {
+						return props.objects[idx].map(obj => {
+							return (
+								<tr key={obj._id}>
+									{
+										props.keys[idx].map(key => {
+											return (<td key={key}>{obj[key]}</td>);
+										})
+									}
+								</tr>
+							);
+						});
 					})
 				}
-				</tr>
-			);
-        })
-		}
-      </tbody>
-    </table>
-    </div>
-     
-    );
+			</tbody>
+		</table>
+	</div>
+
+	);
 }
 
-export default function viewusers()  {
-    const [users, setUsers] = useState([]);
-    const [proposals, setProposals] = useState([]);
-    const [keys,setKeys] = useState([]);
-    const [proposalKeys, setProposalKeys] = useState([]);
-    const fetchData = () => {
-    	fetch("http://localhost:3000/api/project")
-		.then(response => response.json())
-        .then(jsondata => {
-        	console.log("Gold Jagz");
-        	setUsers(JSON.parse(JSON.stringify(jsondata)));
-        	var proposal_objs = jsondata.map(
-    			function (project){
-    				return project.proposal;
-    			}
-    		)
-        	setProposals(proposal_objs);
-        	const main_fields = [ 
-		    	"_id", 
-		    	"approved_budget",
-		    	"approved_duration",
-		    	"completed_on", 
-				"status_updates",
-				"outcomes"
-			];
-			// TODO: add member names
-			const sub_fields = [
-				"domains",
-				"funding_type",
-				"funding_agency",
-			];
-        	setKeys(main_fields);
-        	setProposalKeys(sub_fields);
-        	
-        })
+export default function viewusers() {
+	const [projects, setProjects] = useState([]);
+	const [proposals, setProposals] = useState([]);
+	const [keys, setKeys] = useState([]);
+	const [proposalKeys, setProposalKeys] = useState([]);
+	const fetchData = () => {
+		fetch("http://localhost:3000/api/project")
+			.then(response => response.json())
+			.then(jsondata => {
+				console.log("Gold Jagz");
+				setProjects(JSON.parse(JSON.stringify(jsondata)));
+				var proposal_objs = jsondata.map(
+					function (project) {
+						return project.proposal;
+					}
+				)
+				setProposals(proposal_objs);
+				const main_fields = [
+					"_id",
+					"approved_budget",
+					"approved_duration",
+					"completed_on",
+					"status_updates",
+					"outcomes"
+				];
+				// TODO: add member names
+				const sub_fields = [
+					"domains",
+					"funding_type",
+					"funding_agency",
+				];
+				setKeys(main_fields);
+				setProposalKeys(sub_fields);
+
+			})
 	}
-    
 
-        useEffect(() => {
-          fetchData()
-        }, []);
-        
-        // Todo: Make objects multiple array sets
-        let temp = {"objects":[users,users],"keys":[keys], "link_objects":1};
 
-return (
-    <div id="vieusers">
-    <Header></Header>
-    <Tabulate props={temp}/>;
-    </div>
+	useEffect(() => {
+		fetchData()
+	}, []);
 
-);
+	console.log("CHECK");
+	console.log([projects, projects]);
+	// Todo: Make objects multiple array sets
+	let temp = { "objects": [projects, proposals], "keys": [keys, proposalKeys], "link_objects": 1 };
+
+	return (
+		<div id="vieusers">
+			<Header></Header>
+			<Tabulate props={temp} />;
+		</div>
+
+	);
 
 }
