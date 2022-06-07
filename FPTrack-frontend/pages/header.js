88f6a00b54179bdb8ed9a6a55c6484f244a1b0bf
karthik-d <karthikdesingu2000@gmail.com>
2022-06-07 23:40:30 +0530

    auth src moved

--- FPTrack-frontend/pages/header.js
+++ FPTrack-frontend/pages/header.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import GoogleSignin from '../src/components/auth/google_oauth'
+import GoogleSignin from '../components/auth/google_oauth'
 
 export default function Header() {
   return (
