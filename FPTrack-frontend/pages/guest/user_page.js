09e19760799b82a99b192da002ea627f66905c72
KarthikRaja2k1 <75213811+KarthikRaja2k1@users.noreply.github.com>
2022-06-13 04:31:43 +0530

    created skeleton userpage (#25)
    * next step to create card for project view
    
    * added skeleton project page ,pdf not working
    
    * project page without css, and file name changes
    
    * nothing imp
    
    * nothing imp1
    
    * added resource card, have to update its url
    
    * jags_commit_proposal
    
    * added search box for project view
    
    * da

--- FPTrack-frontend/pages/guest/user_page.js
+++ FPTrack-frontend/pages/guest/user_page.js
@@ -1,27 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../../styles/Home.module.css'
-import Header from '../header'
-import { useRouter } from 'next/router'
-import useSWR from 'swr'
-import FullProject from '../../components/FullProjectCard'
-
-
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import Header from "../header";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import FullUser from "../../components/FullUserCard";
 
 export default function User(props) {
   const fetcher = (url) => fetch(url).then((res) => res.json());
-    const router = useRouter();
-    const { data, error } = useSWR(router.query.data, fetcher)
+  const router = useRouter();
+  const { data, error } = useSWR(router.query.data, fetcher);
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-     // render data
-     console.log(data);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  // render data
+  console.log(data);
   return (
     <div className={styles.container}>
-    <Header></Header>
-    <FullProject props={data[0]}/>
+      <Header></Header>
+      <FullUser props={data[0]} />
     </div>
   );
 }
