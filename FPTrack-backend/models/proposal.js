bd395c880d40599a5ff6c43554c1fe36e368050f
Karthik <Karthikraja2k1@gmail.com>
2022-06-14 02:10:15 +0530

    resrc adder

--- FPTrack-backend/models/proposal.js
+++ FPTrack-backend/models/proposal.js
@@ -8,92 +8,71 @@ var Schema = mongoose.Schema;
 // - setup a trigger/autorun to make on or the other null, if the other is forcefully non-nulled
 // - (add on...)
 var ProposalSchema = new Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            maxLength: 100,
+  {
+    title: {
+      type: String,
+      required: true,
+      maxLength: 100,
+    },
+    description: {
+      type: String,
+    },
+    domains: {
+      type: [
+        {
+          type: String,
+          enum: [
+            "machine_learning",
+            "web_development",
+            "iot",
+            "computer_vision",
+            "nlp",
+            "cybersecurity",
+          ],
         },
-        description: {
-            type: String,
+      ],
+      required: true,
+    },
+    supervisors: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "user",
         },
-        domains: {
-            type: [
-                {
-                    type: String,
-                    enum: [
-                        "machine_learning",
-                        "web_development",
-                        "iot",
-                        "computer_vision",
-                        "nlp",
-                        "cybersecurity",
-                    ],
-                },
-            ],
-            required: true,
+      ],
+      validate: {
+        validator: function (given_proj) {
+          var current_proposal = this;
+          return new Promise(function (resolve, reject) {
+            current_proposal.populate("supervisors").then((populated_data) => {
+              var allFaculties = true;
+              var supervisorsList = populated_data.supervisors;
+              supervisorsList.forEach((supervisor) => {
+                console.log(supervisor.role);
+                if (supervisor.role != "faculty") {
+                  allFaculties = false;
+                }
+              });
+              return resolve(allFaculties);
+            });
+          });
         },
-        supervisors: {
-            type: [
-                {
-                    type: Schema.Types.ObjectId,
-                    ref: "user",
-                },
-            ],
-            validate: {
-                validator: function (given_proj) {
-                    var current_proposal = this;
-                    return new Promise(function (resolve, reject) {
-                        current_proposal.populate("supervisors").then((populated_data) => {
-                            var allFaculties = true;
-                            var supervisorsList = populated_data.supervisors;
-                            supervisorsList.forEach((supervisor) => {
-                                console.log(supervisor.role);
-                                if (supervisor.role != "faculty") {
-                                    allFaculties = false;
-                                }
-                            });
-                            return resolve(allFaculties);
-                        });
-                    });
-                },
-                message: (props) => `All supervisors must be faculty`,
-            },
-            required: true,
+        message: (props) => `All supervisors must be faculty`,
+      },
+      required: true,
+    },
+    // the user type of leader determines if project is Student project or Faculty project
+    leader: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    members: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "user",
         },
-        // the user type of leader determines if project is Student project or Faculty project
-        leader: {
-            type: Schema.Types.ObjectId,
-            ref: "user",
-            required: true,
-        },
-        members: {
-            type: [
-                {
-                    type: Schema.Types.ObjectId,
-                    ref: "user",
-                },
-<<<<<<< HEAD
-                rejected_on: {
-                    type: Date,
-                    default: null,
-                    validate: {
-                        validator: function (given_proj) {
-                            var current_proposal = this;
-                            return new Promise(
-                                function (resolve, reject) {
-                                    var result = true;
-                                    if (current_proposal.rejected_on != null && current_proposal.approved_on != null) {
-                                        result = false;
-                                    }
-                                    // console.log(result)
-                                    return resolve(result);
-                                }
-                            );
-                        },
-                        message: props => `Either rejected or approved date should be mentioned but not both!`
-                    },
-=======
       ],
       validate: {
         validator: function (given_proj) {
@@ -115,146 +94,142 @@ var ProposalSchema = new Schema(
               return resolve(true);
             }
           });
->>>>>>> b650554ef47375993691afe3be9daf8dbc73c8c3
-                },
-                message: (props) =>
-                    `Duplicate entry for leader found in members list. Mention only once`,
+        },
+        message: (props) =>
+          `Duplicate entry for leader found in members list. Mention only once`,
       },
-        required: true,
+      required: true,
     },
     funding_type: {
-    type: String,
-    enum: ["internal", "external"],
-    required: true,
-},
+      type: String,
+      enum: ["internal", "external"],
+      required: true,
+    },
     // TODO: Convert this to an enum type with possibility of adding new fuding agencies??
     funding_agency: {
-    type: String,
-    required: true,
-},
+      type: String,
+      required: true,
+    },
     // Accepts file sizes upto 16MB -- Indicate limit as 8MB on Frontend
     // Transit as Base64 string on JSON
     pdf_document: {
-    type: Buffer,
-    required: false,
-},
+      type: Buffer,
+      required: false,
+    },
     budget: {
-    type: Number,
-    required: true,
-},
+      type: Number,
+      required: true,
+    },
     approved_on: {
-    type: Date,
-    default: null,
-},
+      type: Date,
+      default: null,
+    },
     rejected_on: {
-    type: Date,
-    default: null,
-    validate: {
+      type: Date,
+      default: null,
+      validate: {
         validator: function (given_proj) {
-            var current_proposal = this;
-            return new Promise(function (resolve, reject) {
-                var result = true;
-                if (
-                    current_proposal.rejected_on != null &&
-                    current_proposal.approved_on != null
-                ) {
-                    result = false;
-                }
-                // console.log(result)
-                return resolve(result);
-            });
+          var current_proposal = this;
+          return new Promise(function (resolve, reject) {
+            var result = true;
+            if (
+              current_proposal.rejected_on != null &&
+              current_proposal.approved_on != null
+            ) {
+              result = false;
+            }
+            // console.log(result)
+            return resolve(result);
+          });
         },
         message: (props) =>
-            `Either rejected or approved date should be mentioned but not both!`,
+          `Either rejected or approved date should be mentioned but not both!`,
+      },
+      message: (props) =>
+        `Either rejected or approved date should be mentioned but not both!`,
     },
-},
     rejection_remarks: {
-    type: String,
-    default: null,
-},
-    deleted_at: {
-    type: Date,
-    default: null,
-},
-  },
-{
-    timestamps: {
-        created_at: "created_at",
-            modified_at: "modified_at",
+      type: String,
+      default: null,
     },
-}
+    deleted_at: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: {
+      created_at: "created_at",
+      modified_at: "modified_at",
+    },
+  }
 );
 
 // Chain <ModelName>.onlyExisting before any query to list only "non-deleted" records
 ProposalSchema.statics.onlyExisting = function () {
-    return this.find().onlyExisting();
+  return this.find().onlyExisting();
 };
 
 ProposalSchema.query.onlyExisting = function () {
-    return this.find({
-        deleted_at: null,
-    });
+  return this.find({
+    deleted_at: null,
+  });
 };
 
 // --
 
-ProposalSchema.statics.getById = function (id) {
-    return this.find().getById();
+ProposalSchema.statics. Id = function (id) {
+  return this.find().getById();
 };
 
 ProposalSchema.query.getById = function (id) {
-    return this.find({
-        _id: id,
-    });
+  return this.find({
+    _id: id,
+  });
 };
 
 // --
 
 ProposalSchema.methods.isApproved = function () {
-    return this.rejected_on != null && this.approved_on == null;
+  return this.rejected_on != null && this.approved_on == null;
 };
 
 ProposalSchema.methods.isRejected = function () {
-    return this.rejected_on == null && this.approved_on != null;
+  return this.rejected_on == null && this.approved_on != null;
 };
 
 ProposalSchema.methods.isAwaitingDecision = function () {
-    return this.rejected_on == null && this.approved_on == null;
+  return this.rejected_on == null && this.approved_on == null;
 };
 
 // --
 
 // virtual for executive members
 ProposalSchema.virtual("executive_members").get(function () {
-    var all_members = [];
-    all_members = all_members.concat([leader], members);
-    return all_members;
+  var all_members = [];
+  all_members = all_members.concat([leader], members);
+  return all_members;
 });
 
 // virtual for executive members
-ProposalSchema
-    .virtual('all_members')
-    .get(function () {
-        var all_members = [];
-        all_members = all_members.concat(executive_members, supervisors);
-        return all_members;
-    });
+ProposalSchema.virtual("all_members").get(function () {
+  var all_members = [];
+  all_members = all_members.concat(executive_members, supervisors);
+  return all_members;
+});
 
-ProposalSchema
-    .virtual('document_base64')
-    .get(function () {
-        console.log(this.pdf_document);
-        if (this.pdf_document) {
-            return Buffer.from(this.pdf_document).toString('base64');
-        }
-        else {
-            return null;
-        }
-    })
+ProposalSchema.virtual("document_base64").get(function () {
+  console.log(this.pdf_document);
+  if (this.pdf_document) {
+    return Buffer.from(this.pdf_document).toString("base64");
+  } else {
+    return null;
+  }
+});
 
 // virtual for user URL
 ProposalSchema.virtual("url").get(function () {
-    return "/api/proposal/" + this._id;
+  return "/api/proposal/" + this._id;
 });
 
 //Export model
