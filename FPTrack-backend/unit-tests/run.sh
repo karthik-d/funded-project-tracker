1f20d951458a3dd6f05070ed74c1dc714b0a5a81
Jagadish <goldjagadish@gmail.com>
2022-06-14 08:59:02 +0530

    added uni test cases

--- FPTrack-backend/unit-tests/run.sh
+++ FPTrack-backend/unit-tests/run.sh
@@ -1,2 +1,8 @@
-echo "Testing Proposals..."
-python -m unittest ProposalTests.py -v
+# echo "Testing Proposals..."
+# python -m unittest ProposalTests.py -v
+
+# echo "Testing Projects..."
+# python -m unittest ProjectTests.py -v
+
+echo "Testing Resource Assignments..."
+python3 -m unittest ResourceAssignmentTests.py -v
